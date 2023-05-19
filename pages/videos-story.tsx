25a22f60d8a144b04f20d68391a181888dc10afc
letho0119 <letho0119@gmail.com>
2023-05-19 16:46:37 -0400

    chore: update videos

--- pages/videos-story.tsx
+++ pages/videos-story.tsx
@@ -4,28 +4,38 @@ import StoriesLazy from '@/components/story'
 import LinkSVG from "@/assets/link.svg";
 const stories2 = [
   {
-    playbackId: "Fo4004ggpHhxpLoBmiJ5302VQAUJF7qTZKzFhwuh9Y9Wk",
-    duration: 10 * 1000,
-    type: "mux-video",
-  },
-  {
-    playbackId: "sbZHpPgR00501ZNCqVimHwezVyGoWA5tBUgctwAtva3uQ", // groundbreak is short form video
-    type: "mux-video",
-    duration: 7 * 1000,
-  },
-  {
-    playbackId: "kxPkolm1cFhR02eaDkESrBVxeNSuHdfs9Z1TAiPDTIfo", // groundbreak is short form video
-    type: "mux-video",
+    playbackId: "4vipwgRq4p01pYskY5oGjM02fwL6kGA00eG53ux02aQRl5w",
     duration: 9 * 1000,
-  },
-  {
-    playbackId: "UB02uduyMugRkGVvNHypNQszUdQ4300Qfw7UnmHgE3dME", // best sales tool
-    duration: 5 * 1000,
     type: "mux-video",
   },
   {
-    playbackId: "XJDBT01jYRQCBmKplsYGB23KASPbdEc6HnTV4oOQPlJA",
-    duration: 8 * 1000,
+    playbackId: "SmaCpxZUcHzrwE6qIuIzyrjwWSPy00OFWExu4waY2WA8", // groundbreak is short form video
+    type: "mux-video",
+    duration: 5 * 1000,
+  },
+  {
+    playbackId: "6Rask9izbjGHMBGZiFS5xMoEDV2TYB6of27cuqpvIZk", // groundbreak is short form video
+    type: "mux-video",
+    duration: 22 * 1000,
+  },
+  {
+    playbackId: "DhjdILWdsQSoqUs2ps9nFLs02K00c4Dx2hP1Iyemd8oAw", // best sales tool
+    duration: 25 * 1000,
+    type: "mux-video",
+  },
+  {
+    playbackId: "AAjzwXeiO551UAvjID8fPS02bESiRX8jwyqUj024gcA28",
+    duration: 15 * 1000,
+    type: "mux-video",
+  },
+  {
+    playbackId: "I01aOPryQi5hVuir02mqM02B3G01g45s00EFDaGzqlS3bQY4",
+    duration: 11 * 1000,
+    type: "mux-video",
+  },
+  {
+    playbackId: "00CzDwETt01Sh1eNqIgp4DM01008SqWdqbeGEIkkAITagp8",
+    duration: 10 * 1000,
     type: "mux-video",
   },
   {
